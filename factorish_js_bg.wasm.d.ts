63ba7a255050cec70d6bc7a01385bc2e27d36bfa
msakuta <msakuta@users.noreply.github.com>
2020-11-08 14:21:35 +0000

    deploy: de359a1f20e2d048f3006499782fe14b7d7e7123

---
+++ factorish_js_bg.wasm.d.ts
@@ -0,0 +1,30 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function greet(): void;
+export function __wbg_factorishstate_free(a: number): void;
+export function factorishstate_new(a: number): number;
+export function factorishstate_simulate(a: number, b: number): number;
+export function factorishstate_get_player_inventory(a: number): number;
+export function factorishstate_select_player_inventory(a: number, b: number, c: number): void;
+export function factorishstate_open_structure_inventory(a: number, b: number, c: number): void;
+export function factorishstate_get_selected_inventory(a: number): number;
+export function factorishstate_get_structure_inventory(a: number, b: number, c: number): number;
+export function factorishstate_select_structure_inventory(a: number, b: number, c: number): void;
+export function factorishstate_move_selected_inventory_item(a: number, b: number): number;
+export function factorishstate_mouse_down(a: number, b: number, c: number, d: number): number;
+export function factorishstate_mouse_move(a: number, b: number, c: number): void;
+export function factorishstate_mouse_leave(a: number): void;
+export function factorishstate_on_key_down(a: number, b: number): number;
+export function factorishstate_render_init(a: number, b: number, c: number): void;
+export function factorishstate_tool_defs(a: number): number;
+export function factorishstate_selected_tool(a: number): number;
+export function factorishstate_render_tool(a: number, b: number, c: number): void;
+export function factorishstate_select_tool(a: number, b: number): number;
+export function factorishstate_rotate_tool(a: number): number;
+export function factorishstate_tool_inventory(a: number): number;
+export function factorishstate_render(a: number, b: number): void;
+export function perlin_noise_pixel(a: number, b: number, c: number): number;
+export function __wbindgen_malloc(a: number): number;
+export function __wbindgen_realloc(a: number, b: number, c: number): number;
+export function __wbindgen_exn_store(a: number): void;
